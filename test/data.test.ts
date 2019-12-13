c77dc052da90cd44e2213dee5f7ed31b6267985d
Rafael Oliveira <rafael@muara.com.br>
2019-12-13 01:52:23 -0300

    altera descrição do item de teste

--- test/data.test.ts
+++ test/data.test.ts
@@ -6,11 +6,11 @@ const expect = chai.expect; // eslint-disable-line
 describe("Data", () => {
     new Data;
 
-    it("should have a City", () => {    
+    it("expect have a City", () => {    
         expect(Data.data.City).to.not.be.empty;
     });
     
-    it("should have a Weather", () => {
+    it("expect have a Weather", () => {
         expect(Data.data.Weather).to.not.be.empty;
     });
 });
